b5edddd8c4aab3055e5d2bb0e4b842f0879404a7
strangeal <bajerijustice@gmail.com>
2022-11-08 04:57:03 -0500

    Add react redux

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,16 +1,31 @@
-const BookForm = () => (
-  <div className="form__container">
-    <form>
-      <h2>ADD NEW BOOK</h2>
-      <div className="form__group">
-        <input type="text" name="title" placeholder="Book title" />
-      </div>
-      <div className="form__group">
-        <input type="text" name="author" placeholder="Book author" />
-      </div>
-      <button type="submit">ADD BOOK</button>
-    </form>
-  </div>
-);
+import { useDispatch } from 'react-redux';
+import { addBooks } from '../redux/books/books';
 
+const BookForm = () => {
+  // const inputValue = useSelector((state) => (state.books));
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addBooks(e.target.title.value, e.target.author.value));
+  };
+
+  return (
+    <div className="form__container">
+      <form onSubmit={handleSubmit}>
+        <h2>ADD NEW BOOK</h2>
+        <div className="form__group">
+          <input type="text" name="title" placeholder="Book title" />
+        </div>
+        <div className="form__group">
+          <input type="text" name="author" placeholder="Book author" />
+        </div>
+        <button type="submit">
+          ADD BOOK
+        </button>
+      </form>
+    </div>
+  );
+};
+// onChange={(e) => dispatch(addBooks(e.target.value))}
 export default BookForm;
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,15 +1,48 @@
+import { v4 as uuidv4 } from 'uuid';
+
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+// const INPUT_VALUE = 'bookstore/books/INPUT_VALUE';
 
-const initState = [];
+const initState = [
+  {
+    id: uuidv4(),
+    title: 'Rethinking Productivity in Software Engineering',
+    author: 'Caitlin Sadowski, Thomas Zimmermann',
+  },
+  {
+    id: uuidv4(),
+    title: 'Pro Git',
+    author: 'Scott Chacon and Ben Straub',
+  },
+  {
+    id: uuidv4(),
+    title: "You Don't Know JS Yet",
+    author: 'Kyle Simpson',
+  },
+  {
+    id: uuidv4(),
+    title: 'Learning JavaScript Design Patterns',
+    author: 'Addy Osmani',
+  },
+  {
+    id: uuidv4(),
+    title: 'Understanding ECMAScript 6',
+    author: 'Nicholas C. Zakas',
+  },
+];
 
 const booksReducer = (state = initState, action = {}) => {
   switch (action.type) {
     case ADD_BOOK:
-      return {
+      return [
         ...state,
-        REMOVE_BOOK: [...state, action.book],
-      };
+        {
+          id: uuidv4(),
+          title: action.payload.title,
+          author: action.payload.author,
+        },
+      ];
 
     case REMOVE_BOOK:
       return state.filter((book) => (book.id) !== action.id);
@@ -19,8 +52,14 @@ const booksReducer = (state = initState, action = {}) => {
   }
 };
 
-export const addBooks = (book) => ({ type: ADD_BOOK, book });
+export const addBooks = (title, author) => ({
+  type: ADD_BOOK,
+  payload: {
+    title,
+    author,
+  },
+});
 
-export const removeBooks = (book) => ({ type: REMOVE_BOOK, book });
+export const removeBooks = (id) => ({ type: REMOVE_BOOK, id });
 
 export default booksReducer;
